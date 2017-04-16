f070c3ae63ce43c74c0c5942e4bb0b46a9c6b8e9
test <422538722@qq.com>
2017-04-16 15:13:02 +0800

    9.2.9
    UINavigationItem定制示例

---
+++ 9.2.9 UINavigationItem定制示例/9.2.9 UINavigationItem定制示例/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  9.2.9 UINavigationItem定制示例
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
